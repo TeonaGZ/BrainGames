5856e2314651241da37a3349516b9421629caca6
Teona Gatsereliya <teonazg@gmail.com>
2023-04-20 16:33:56 +0400

    Add prime-game

---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+import gameLogic from '../index.js';
+import getRandomInt from '../helper-functions.js';
+
+const intro = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 1; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const startRound = () => {
+  const value = getRandomInt();
+  const questionValue = `${value}`;
+  const rightAnswer = isPrime(value) ? 'yes' : 'no';
+  return [questionValue, rightAnswer];
+};
+
+const playPrimeGame = () => gameLogic(intro, startRound);
+
+export default playPrimeGame;
